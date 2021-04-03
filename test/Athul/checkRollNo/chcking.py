5a560b21b26d089a0b138b2f96187ada803ce36d
Athul-das7 <75476124+Athul-das7@users.noreply.github.com>
2021-04-03 22:10:33 +0530

    Check the roll number is valid or not
    This is a base of this function because the data from the barcode is still not know. However this is serves as a good pseudo code

--- test/Athul/checkRollNo/chcking.py
+++ test/Athul/checkRollNo/chcking.py
@@ -1,1 +1,31 @@
-:
+brsc = Barcodedata    #Brsc holds the entire data scanned by barcode scanner 
+ans = True
+#since I don't know the form of data we are going to recieve through the barcode scanner lets be abstract for the time being 
+
+if brsc.barCodeType != 'Code 128':
+     ans = False
+
+#roll nos are from is in the form of 1602-yy-BRN-RNO 
+# yy - year; BRN - branch id; RNO - roll number
+
+rollno=brsc.data.split('-')
+#splits roll no to list for easier checking
+
+known = [ 1602, [20,19,18,17], [ 732, 733, 734, 735, 736, 737 ] ]
+#The inital data...or the range in which the data should be
+
+for i in range(4):
+    if i == 3 :
+        if rollno[i] < 0 or rollno[i] > 180:
+            ans = False
+            break
+    if ans == False :
+        break
+    if rollno[i] not in known[i]:
+        ans = False
+
+if ( ans == False  ):
+    #rerun the barcode
+    #return ans 
+    # if ans is true
+    # proceed for check distance function
