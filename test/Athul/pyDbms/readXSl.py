c0f46750ae5b41e85e65a84a21184183767e3ab0
Athul-das7 <75476124+Athul-das7@users.noreply.github.com>
2021-05-02 21:12:50 +0530

    Images for the dbms and The database
    I used an automated script using python to make our inserts for our database. Now its is easy for us to create our database
    Also I have added all the images required for our database.
    The excel sheet is also available for further reference.

---
+++ test/Athul/pyDbms/readXSl.py
@@ -0,0 +1,31 @@
+#Reads the excel and creates a database type script
+import pandas as pd
+
+sheet = pd.read_excel('data.xlsx')
+#sheet.drop(index='Name of the College: Vasavi College of Engineering (Autonomous)')
+f = open('sql.txt','a')
+#print(sheet)
+b = 'gotchya'
+for i in range(len(sheet)):
+    num = sheet.iloc[i,0]
+    #print(type(rn))
+    rn = num
+    nm = sheet.iloc[i,2]
+    #print(rn)
+    if rn == 1 :
+        b = 'A'
+    elif rn is 61 :
+        b = 'B'
+    elif rn is 121 :
+        b = 'C'
+    #print(b)
+    if isinstance(rn,int):
+        if rn//10 == 0:
+            rn = str(rn)
+            rn = '00'+str(rn)
+        elif rn//100 == 0:
+            rn = str(rn)
+            rn = '0' + str(rn)
+
+#with open('sql.txt','a') as f:
+    f.write(f'("1602-19-735-{rn}","{nm}","ECE","{b}","media\\\\{num}.jpg"),\n')
