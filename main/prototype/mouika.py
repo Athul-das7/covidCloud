a6962a1dcc790c873e24e1c35511a428c5cace48
Athul-das7 <75476124+Athul-das7@users.noreply.github.com>
2021-05-12 21:18:43 +0530

    Changes to mounika.py

--- main/prototype/mouika.py
+++ main/prototype/mouika.py
@@ -3,12 +3,12 @@ import prototypeCloud as pc
 import time
 import gui3 as G
 
-while True:
+while True:      #It must always run
     test = p.covidCloud()
     test1 = pc.cloudPrediction()
     testgui=G.details()
     testgui.message1()
-    rnum = test.readBarcode()
+    rnum = test.readBarcode()       #the rollno must be entered in the terminal
     print(rnum)
     test.checkRollNo(rnum)
     detail=test.readDbms(rnum)
@@ -26,6 +26,31 @@ while True:
     test.checkRollNo(rnum)
     detail=test.readDbms(rnum)
     print(detail)
+    print("media\\{}".format(detail[4]))
+    print(f'Name:  {detail[1]}\nRoll No.:  {detail[0]}\nBranch:  {detail[2]} {detail[3]}')
+    testgui.det_image(detail)
+
+    time.sleep(3)
+    ckdis = False
+    dist = 2.00
+    temp=0
+    while ckdis!=True:
+        dist = test.readDistance()
+
+        print(dist, " cm")
+        ckdis = test.checkDistance(dist)
+        if ckdis == True:
+            temp=test.readTemperature()
+            testgui.det_temp(detail,temp)
+            print(test.checkTemperature(temp))
+            if test.checkTemperature(temp):
+                print("Hello world")
+        else:
+            testgui.message3()
+            print(" please put your hand closer to the sensor")
+            time.sleep(2)
+            continue
+
     print("media\\{}".format(detail[4]))
     print(f'Name:  {detail[1]}\nRoll No.:  {detail[0]}\nBranch:  {detail[2]} {detail[3]}')
     testgui.det_image(detail)
