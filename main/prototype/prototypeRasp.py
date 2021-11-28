9e917e3627518a62fc7bd9dc7a425c3e14aa14cf
Athul Das <athuldas2017@gmail.com>
2021-11-28 21:11:39 +0530

    Update prototypeRasp.py

--- main/prototype/prototypeRasp.py
+++ main/prototype/prototypeRasp.py
@@ -30,40 +30,13 @@ from gpiozero import Buzzer
 
 class covidCloud:
     def readBarcode(self):
-                # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-        # other example, but it includes some basic performance tweaks to make things run a lot faster:
-        #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-        #   2. Only detect faces in every other frame of video.
-
-        # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-        # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-        # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-        # Get a reference to webcam #0 (the default one)
         video_capture = cv2.VideoCapture(0)
 
-        # Load a sample picture and learn how to recognize it.
-        # obama_image = face_recognition.load_image_file("athul.jpg")
-        # obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-        # # Load a second sample picture and learn how to recognize it.
-        # biden_image = face_recognition.load_image_file("mouni.jpeg")
-        # biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-        # Create arrays of known face encodings and their names
         with open(f"Encodings.pickle",'rb') as file:
             known_face_encodings = pickle.load(file)
-        # = [
-        #     obama_face_encoding,
-        #     biden_face_encoding
-        # ]
+
         known_face_names = list(known_face_encodings.keys())
         known_face_encodings = np.array(list(known_face_encodings.values()))
-        # = [
-        #     "Athul Das",
-        #     "Mounika"
-        # ]
-
         # Initialize some variables
         face_locations = []
         face_encodings = []
@@ -71,12 +44,11 @@ class covidCloud:
         process_this_frame = True
         past = time.time()
         now = time.time()
+        name=""
 
-        while now-past > 5:
+        while True:
             # Grab a single frame of video
             ret, frame = video_capture.read()
-
-            # Resize frame of video to 1/4 size for faster face recognition processing
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
@@ -94,12 +66,6 @@ class covidCloud:
                     matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                     name = "Unknown"
 
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    # if True in matches:
-                    #     first_match_index = matches.index(True)
-                    #     name = known_face_names[first_match_index]
-
-                    # Or instead, use the known face with the smallest distance to the new face
                     face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
@@ -107,6 +73,11 @@ class covidCloud:
 
                     face_names.append(name)
                     print(name)
+                    if "1602" in name:
+                        video_capture.release()
+                        cv2.destroyAllWindows()
+                        return name
+
 
             process_this_frame = not process_this_frame
 
@@ -221,9 +192,9 @@ class covidCloud:
         c = 4
         while (c):
             buzzer.on()
-            sleep(1)
+            time.sleep(1)
             buzzer.off()
-            sleep(1)
+            time.sleep(1)
             c -= 1
 
 
